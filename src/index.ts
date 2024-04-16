555c49a7229bf25c517050f80af8bf7c73156c4c
Muhammad Saqib Arfeen <saqibarfeen@gmail.com>
2024-04-16 17:43:14 +0200

    [ND-7872] started

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { Command } from "commander";
+
+const figlet = require("figlet");
+
+
+
+const program = new Command();
+
+
+// console.log(figlet.textSync("Ninox CLI SDK"));
+
+
+program
+    .version("0.0.1")
+    .description("Ninox CLI SDK")
+    .command("init", "Initialize a new Ninox SDK project",{executableFile: "init", })
+    // .command("deploy", "Deploy the current project to Ninox")
+    // .command("object:import", "Import an object to Ninox")
+    .parse(process.argv);
+
+const options = program.opts();
+console.log(options);
