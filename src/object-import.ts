40eb96af754b8e9928b5822f2fac1d39f62e14bf
Muhammad Saqib Arfeen <saqibarfeen@gmail.com>
2024-04-17 12:27:23 +0200

    poc: using zod

--- src/object-import.ts
+++ src/object-import.ts
@@ -1,4 +1,5 @@
 import { Command } from "commander";
+import { doSOmething } from "./common/doParsingTest";
 
 const objectImport = new Command("object:import");
 
@@ -7,6 +8,11 @@ objectImport
   .option("-t, --type <type>", "Object Type e.g Database, Table, View, Field")
   .option("-id, --id <id>", "Object ID")
   .action((options) => {
-    console.log(`Project importialized with type: ${options.type} and id: ${options.id}`);
+    console.log(
+      `Project importialized with type: ${options.type} and id: ${options.id}`
+    );
+    doSOmething(options);
   })
   .parse(process.argv);
+
+
