c09e251b88edc25cc19c2485a997bd7986761b54
Muhammad Saqib Arfeen <saqibarfeen@gmail.com>
2024-04-17 10:30:19 +0200

    import command added

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ program
     .version("0.0.1")
     .description("Ninox CLI SDK")
     .command("init", "Initialize a new Ninox SDK project",{executableFile: "init", })
+    .command("object:import", "Import an object from Ninox",{executableFile: "object-import" })
     // .command("deploy", "Deploy the current project to Ninox")
     // .command("object:import", "Import an object to Ninox")
     .parse(process.argv);
---
+++ src/object-import.ts
@@ -0,0 +1,12 @@
+import { Command } from "commander";
+
+const objectImport = new Command("object:import");
+
+objectImport
+  .description("Import an object from Ninox")
+  .option("-t, --type <type>", "Object Type e.g Database, Table, View, Field")
+  .option("-id, --id <id>", "Object ID")
+  .action((options) => {
+    console.log(`Project importialized with type: ${options.type} and id: ${options.id}`);
+  })
+  .parse(process.argv);
