40c375477531239211ad522f9ade2e770cfa1180
Muhammad Saqib Arfeen <saqibarfeen@gmail.com>
2024-05-08 15:09:05 +0200

    [feat] Download command completed and tested

--- test/commands/download.test.ts
+++ test/commands/download.test.ts
@@ -1,17 +1,47 @@
 import {expect, test} from '@oclif/test'
+import nock from 'nock'
+import sinon from 'sinon'
+
+import DownloadCommand from '../../src/commands/download.js'
+import {loadJsonMock} from '../common/test-utils.js'
 
 describe('download', () => {
-  test
-  .stdout()
-  .command(['download'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
+  let stubReadEnvironmentConfig: sinon.SinonStub
+  const dbId = '4321'
+  before(() => {
+    const databaseJSONMock = loadJsonMock('download-database-info.json')
+    // const databaseJSONMock =  require('../mocks/database-info.json');
+    nock('https://mocked.example.com')
+      .get(`/v1/teams/mocked-workspace/databases/${dbId}?human=T`)
+      .reply(200, databaseJSONMock)
+
+    nock('https://mocked.example.com').get(`/mocked-workspace/${dbId}/files/background.jpg`).reply(200, 'mocked-image')
+
+    stubReadEnvironmentConfig = sinon.stub(DownloadCommand.prototype, 'readEnvironmentConfig').callsFake(() => ({
+      apiKey: 'mocked-api-key',
+      domain: 'https://mocked.example.com',
+      workspaceId: 'mocked-workspace',
+    }))
+  })
+
+  after(() => {
+    stubReadEnvironmentConfig.restore()
   })
 
   test
-  .stdout()
-  .command(['download', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
+    .stdout()
+    .command(['download', '--id', dbId])
+    .it('runs download', (ctx) => {
+      expect(ctx.stdout).to.contain(`Downloaded database ${dbId} successfully!`)
+    })
+  test
+    .stderr()
+    .command(['download', dbId])
+    .catch((error) => {
+      expect(error.message).to.contain(`Unexpected argument: ${dbId}\nSee more help with --help`)
+      expect((error as any).oclif.exit).to.equal(2)
+    })
+    .it(`Should throw exception when run download with argument instead of flag i.e download ${dbId}`, (ctx) => {
+      console.log(ctx.stderr)
+    })
 })
