5568c7911ac7890c67e71521899681f0a2c57d05
Muhammad Saqib Arfeen <saqibarfeen@gmail.com>
2024-05-08 13:16:21 +0200

    [fix] refactored FSUtil for furthur testability

--- src/core/utils/fs-util.ts
+++ src/core/utils/fs-util.ts
@@ -6,29 +6,44 @@ import {CREDENTIALS_FILE_NAME, ConfigYamlTemplate, DB_BACKGROUND_FILE_NAME} from
 import {DBConfigsYaml} from '../common/typings.js'
 import {createYamlDocument} from './yaml-util.js'
 
-const ObjectsPath = path.join(process.cwd(), 'src', 'Objects')
-const FilesPath = path.join(process.cwd(), 'src', 'Files')
-const credentialsFilePath = path.join(process.cwd(), CREDENTIALS_FILE_NAME)
-
 export class FSUtil {
+  static _credentialsFilePath = path.join(process.cwd(), CREDENTIALS_FILE_NAME)
+
+  static _filesPath = path.join(process.cwd(), 'src', 'Files')
+
+  static _objectsPath = path.join(process.cwd(), 'src', 'Objects')
+
+  static get credentialsFilePath(): string {
+    return FSUtil._credentialsFilePath
+  }
+
+  static get filesPath(): string {
+    return FSUtil._filesPath
+  }
+
+  static get objectsPath(): string {
+    return FSUtil._objectsPath
+  }
+
+  // eslint-disable-next-line perfectionist/sort-classes
   public static createConfigYaml = async () => {
-    if (fs.existsSync(credentialsFilePath)) {
+    if (fs.existsSync(this.credentialsFilePath)) {
       return
     }
 
-    await fsAsync.writeFile(credentialsFilePath, createYamlDocument(ConfigYamlTemplate).toString())
+    await fsAsync.writeFile(this.credentialsFilePath, createYamlDocument(ConfigYamlTemplate).toString())
   }
 
   public static createDatabaseFolderInFiles = async (databaseId: string) => {
     // create folder src/Files/Database_${databaseid}
-    await fsAsync.mkdir(path.join(FilesPath, `Database_${databaseId}`), {
+    await fsAsync.mkdir(path.join(this.filesPath, `Database_${databaseId}`), {
       recursive: true,
     })
   }
 
   public static createDatabaseFolderInObjects = async (databaseId: string) => {
     // create folder src/Object/Database_${databaseid}
-    await fsAsync.mkdir(path.join(ObjectsPath, `Database_${databaseId}`), {
+    await fsAsync.mkdir(path.join(this.objectsPath, `Database_${databaseId}`), {
       recursive: true,
     })
   }
@@ -54,30 +69,30 @@ export class FSUtil {
   }
 
   public static ensureRootDirectoryStructure = async () => {
-    await fsAsync.mkdir(ObjectsPath, {recursive: true})
-    await fsAsync.mkdir(FilesPath, {recursive: true})
+    await fsAsync.mkdir(this.objectsPath, {recursive: true})
+    await fsAsync.mkdir(this.filesPath, {recursive: true})
   }
 
   public static getDbBackgroundImagePath = (databaseId: string) =>
-    path.join(FilesPath, `Database_${databaseId}`, DB_BACKGROUND_FILE_NAME)
+    path.join(this.filesPath, `Database_${databaseId}`, DB_BACKGROUND_FILE_NAME)
 
   public static getFilePath = (databaseId: string, objectName: string) => {
-    if (!fs.existsSync(path.join(FilesPath, `Database_${databaseId}`))) {
+    if (!fs.existsSync(path.join(this.filesPath, `Database_${databaseId}`))) {
       throw new Error('File path not set')
     }
 
-    return path.join(FilesPath, `Database_${databaseId}`, `${objectName}.yaml`)
+    return path.join(this.filesPath, `Database_${databaseId}`, `${objectName}.yaml`)
   }
 
   public static getObjectFileName = (objectType: string, objectId: string) =>
     `${objectType}_${this.normalizeFileName(objectId)}`
 
   public static getObjectPath = (databaseId: string, objectName: string) => {
-    if (!fs.existsSync(path.join(ObjectsPath, `Database_${databaseId}`))) {
+    if (!fs.existsSync(path.join(this.objectsPath, `Database_${databaseId}`))) {
       throw new Error('Object path not set')
     }
 
-    return path.join(ObjectsPath, `Database_${databaseId}`, `${objectName}.yaml`)
+    return path.join(this.objectsPath, `Database_${databaseId}`, `${objectName}.yaml`)
   }
 
   public static isDatabaseBackgroundFileExist = (databaseId: string) => {
@@ -88,41 +103,41 @@ export class FSUtil {
   public static isProjectInitialized = () => fs.existsSync(path.join(process.cwd(), CREDENTIALS_FILE_NAME))
 
   public static readCredentials = () => {
-    if (!fs.existsSync(credentialsFilePath)) {
+    if (!fs.existsSync(this.credentialsFilePath)) {
       throw new Error('config.yaml file not found')
     }
 
-    return fs.readFileSync(credentialsFilePath, 'utf8')
+    return fs.readFileSync(this.credentialsFilePath, 'utf8')
   }
 
   public static readDefinedDatabaseConfigsFromFiles = async () => {
-    // do a scan of ObjectsPath dir
+    // do a scan of this.objectsPath dir
     // each directory is a isolated database with its own schema, tables and views
     // return an array of database configs
     const databaseConfigs: DBConfigsYaml[] = []
-    if (!fs.existsSync(ObjectsPath)) {
+    if (!fs.existsSync(this.objectsPath)) {
       return databaseConfigs
     }
 
-    const databaseFolders = await fsAsync.readdir(ObjectsPath)
+    const databaseFolders = await fsAsync.readdir(this.objectsPath)
     for (const folder of databaseFolders) {
       if (!folder.startsWith('Database_')) {
         continue
       }
 
       // eslint-disable-next-line no-await-in-loop
-      const files = await fsAsync.readdir(path.join(ObjectsPath, folder))
+      const files = await fsAsync.readdir(path.join(this.objectsPath, folder))
       const databaseFile = files.find((file) => file.startsWith('Database_'))
       if (!databaseFile) {
         throw new Error('Database file not found')
       }
 
       // eslint-disable-next-line no-await-in-loop
-      const database = await fsAsync.readFile(path.join(ObjectsPath, folder, databaseFile), 'utf8')
+      const database = await fsAsync.readFile(path.join(this.objectsPath, folder, databaseFile), 'utf8')
       const tableFiles = files.filter((file) => file.startsWith('Table_') || file.startsWith('Page_'))
       // eslint-disable-next-line no-await-in-loop
       const tables = await Promise.all(
-        tableFiles.map(async (table) => fsAsync.readFile(path.join(ObjectsPath, folder, table), 'utf8')),
+        tableFiles.map(async (table) => fsAsync.readFile(path.join(this.objectsPath, folder, table), 'utf8')),
       )
       databaseConfigs.push({
         database,
